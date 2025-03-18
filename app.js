d946b45b5359dcb66482fc82b46503f3ef1ade7e
Naiana <158007122+naianakr@users.noreply.github.com>
2025-03-18 13:57:10 -0300

    função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo");
+    let elementoListaAmigos = document.getElementById("listaAmigos");
+
+    if (amigo.value.trim() === "") {
+        alert("Por favor, insira um nome.");
+    return;
+    }
+
+    if (listaAmigos.includes(amigo.value)) {
+        alert("Esse nome já foi adicionado.");
+        return;
+    }
+
+    listaAmigos.push(amigo.value);
+
+    if (elementoListaAmigos.textContent === "") {
+        elementoListaAmigos.textContent = amigo.value;
+    } else {
+        elementoListaAmigos.textContent += ", " + amigo.value;
+    }
+
+    amigo.value = "";
+}
